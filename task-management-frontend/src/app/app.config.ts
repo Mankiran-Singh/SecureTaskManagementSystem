1547073b4d5baef67a7b715c34afc08fbc2180ef
Mankiran-Singh <mankiransingh22@gmail.com>
2026-01-12 17:16:26 +0530

    feat: auth interceptors

---
+++ task-management-frontend/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    { provide: HTTP_INTERCEPTORS, useValue: authInterceptor, multi: true },
+  ]
+};
