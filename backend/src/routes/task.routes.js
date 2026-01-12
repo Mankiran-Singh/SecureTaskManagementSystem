57b8db0526ff3ca790e7e1aa0e90375df7d8de0b
Mankiran-Singh <mankiransingh22@gmail.com>
2026-01-12 15:56:44 +0530

    feat: task routes controllet endpomits

---
+++ backend/src/routes/task.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const auth = require("../middlewares/auth.middleware");
+const role = require("../middlewares/role.middleware");
+const inactivity = require("../middlewares/inactivity.middleware");
+const task = require("../controllers/task.controller");
+
+router.use(auth, inactivity);
+
+router.post("/", role("Admin", "Manager"), task.createTask);
+router.get("/", task.getTasks);
+router.put("/:taskId/status", task.updateStatus);
+router.delete("/:taskId", role("Admin"), task.deleteTask);
+
+module.exports = router;
