09dd5d1999b329a58e6dc94c12f4e3e0901833c2
Mankiran-Singh <mankiransingh22@gmail.com>
2026-01-12 16:02:47 +0530

    fix: commonjs issue

--- backend/src/middlewares/inactivity.middleware.js
+++ backend/src/middlewares/inactivity.middleware.js
@@ -1,7 +1,7 @@
-import { findById } from "../models/User.model";
+const User = require("../models/User.model");
 
-export default async (req, res, next) => {
-  const user = await findById(req.user.id);
+module.exports = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
   const diff = Date.now() - new Date(user.lastActivity).getTime();
   if (diff > 10 * 60 * 1000) {
