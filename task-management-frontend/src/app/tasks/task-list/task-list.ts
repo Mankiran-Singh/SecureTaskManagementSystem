25dd9c29b2eb7efb9236a5543854475535a69bca
Mankiran-Singh <mankiransingh22@gmail.com>
2026-01-12 18:49:38 +0530

    fix: tailwind issue task list

--- task-management-frontend/src/app/tasks/task-list/task-list.ts
+++ task-management-frontend/src/app/tasks/task-list/task-list.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { TaskService } from '../../services/task.service';
-import { MatCardModule } from '@angular/material/card';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
-  imports: [MatCardModule, CommonModule],
+  standalone: true,
+  imports: [CommonModule, MatCardModule], 
   templateUrl: './task-list.html',
-  styleUrl: './task-list.css',
+  styleUrls: ['./task-list.css']
 })
-export class TaskList {
+export class TaskList implements OnInit {
+
   tasks: any[] = [];
 
   constructor(private taskService: TaskService) {}
