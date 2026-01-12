21804e9489d62b4c32856a210d49afd400f234d8
Mankiran-Singh <mankiransingh22@gmail.com>
2026-01-12 15:34:07 +0530

    feat(setup): dependencies, app, server, db connection

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const errorHandler = require("./middlewares/error.middleware");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/tasks", require("./routes/task.routes"));
+
+app.use(errorHandler);
+
+module.exports = app;
---
+++ backend/src/config/jwt.js
@@ -0,0 +1,4 @@
+module.exports = {
+  accessTokenExpiry: "5m",
+  refreshTokenExpiry: "15m",
+};
